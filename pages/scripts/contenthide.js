f97bfd219a550b0ed00b7da5b1e87df092d81b92
//Qewertyy// <99824993+Qewertyy@users.noreply.github.com>
2022-10-11 19:00:24 +0000

    UI Modification

--- pages/scripts/contenthide.js
+++ pages/scripts/contenthide.js
@@ -16,11 +16,11 @@ function cardshide() {
    }
 }
 
-function bodyhide() {
-    var x = document.getElementById("body1");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
+// function bodyhide() {
+//     var x = document.getElementById("body1");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
